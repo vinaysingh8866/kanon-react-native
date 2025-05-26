6a79bd32b50e9422d1c2d08e8d9b3637595b30c5
Vinay Singh <vinaysingh8866@gmail.com>
2025-05-26 15:39:16 -0400

    foxes for build

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-    'react-native-builder-bob/babel-preset',
-  ],
+  presets: ['react-native-builder-bob/babel-preset'],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    'babel-plugin-transform-typescript-metadata',
   ],
 };
